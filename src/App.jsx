7512c71cca815bf75d1909093d764510344ff473
Eduard <edik.moskalets@gmail.com>
2024-08-10 11:31:48 +0300

    setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
 import { useState } from 'react'
 import './App.module.css'
 
+import SearchBar from './components/SearchBar/SearchBar';
+import ImageGallery from './components/ImageGallery/ImageGallery';
+import Loader from './components/Loader/Loader';
+import ErrorMessage from './components/ErrorMessage/ErrorMessage';
+import LoadMoreBtn from './components/LoadMoreBtn/LoadMoreBtn';
+import ImageModal from './components/ImageModal/ImageModal';
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,10 @@
+import css from './SearchBar.module.css'
+
+
+const SearchBar = ({ onSearch }) => {
+  return (
+    
+  )
+}
+
+export default SearchBar

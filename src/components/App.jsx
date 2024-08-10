9ff25565ef5321186e05e4e849a1e5f887dac6ce
Eduard <edik.moskalets@gmail.com>
2024-08-10 12:01:09 +0300

    SearchBar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
-import { useState } from 'react'
-import './App.module.css'
+import React from 'react';
+import { useState, useEffect } from 'react';
+import css from './App.module.css';
+import { getImages } from '../images-api';
 
 import SearchBar from './components/SearchBar/SearchBar';
 import ImageGallery from './components/ImageGallery/ImageGallery';
@@ -7,3 +9,10 @@ import Loader from './components/Loader/Loader';
 import ErrorMessage from './components/ErrorMessage/ErrorMessage';
 import LoadMoreBtn from './components/LoadMoreBtn/LoadMoreBtn';
 import ImageModal from './components/ImageModal/ImageModal';
+
+export default function () {
+  
+  return (
+
+  )
+}
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,10 +1,38 @@
-import css from './SearchBar.module.css'
+import css from './SearchBar.module.css';
+import { Field, Form, Formik } from 'formik';
+import { GoSearch } from 'react-icons/go';
+import css from './SearchBar.module.css';
 
 
-const SearchBar = ({ onSearch }) => {
+export default function SearchBar({ onSearch }) {
   return (
-    
-  )
+    <header className={css.searchbar}>
+      <Formik
+        initialValues={{ query: '' }}
+        onSubmit={(values, actions) => {
+          if (values.query.trim() === '') {
+            toast.error('Please enter a keyword of search!');
+            return;
+          }
+          onSearch(values.query);
+          actions.resetForm();
+        }}
+      >
+        <Form className={css.search_panel}>
+          <Field
+            className={css.search_field}
+            type="text"
+            name="query"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+          ></Field>
+          <button className={css.searchBtn} type="submit">
+            <GoSearch />
+          </button>
+          <Toaster position="top-right" reverseOrder={false} />
+        </Form>
+      </Formik>
+    </header>
+  );
 }
-
-export default SearchBar
